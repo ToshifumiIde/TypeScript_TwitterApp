2c0f4812a5510e6fada165e6a120439c8d473473
ToshifumiIde <toshifumiide198765@gmail.com>
2021-01-29 07:59:48 +0900

    create user auth in app by useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./config/firebase";
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      // userが存在すればdispatch内のloginが呼ばれて、userSliceで設定したinitStateに何かしらのuser情報が格納される
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoUrl,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+  return <div className="App"></div>;
+};
 
 export default App;
